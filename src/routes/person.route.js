34cdd0bfe16a767919f354040a368ff94c3b0575
Jhony Rivero <jhony.rivero@hotmail.com>
2020-06-04 11:06:15 -0500

    ep2 parte 1

--- src/routes/person.route.js
+++ src/routes/person.route.js
@@ -7,14 +7,20 @@
  */
 const KoaRouter = require('koa-router')
 const PersonController = require('../controllers/person.controller')
+// LINEA AGREGADA: referenciamos los esquemas declarados
+const personSchemas = require('../schemas/person.schema')
+// LINEA AGREGADA: referenciamos el middleware de validacion de esquemas
+const schemaValidator = require('../utils/schema-validator')
 const router = new KoaRouter({ prefix: '/person' })
 const controller = new PersonController()
-
+// LINEA AGREGADA: creamos una instancia del validador pasandole la parte del request que queremos validar en este caso (params) y el esquema apropiado
+const byIndexValidator = schemaValidator({ params: personSchemas.byIndex })
+// NUEVA LINEA: creamos una instancia del validador pasandole la parte del request que queremos validar en este caso (body) y el esquema apropiado
+const postValidator = schemaValidator({ body: personSchemas.post })
 // GET /person/29
-router.get('person/byIndex', '/:index', controller.getByIndex)
+router.get('person/byIndex', '/:index', byIndexValidator, controller.getByIndex)
 
-// POST
-router.post('person/post', '/', controller.save)
+router.post('person/post', '/', postValidator, controller.save)
 
 // GET
 router.get('person/query', '/', controller.getByQuery)
---
+++ src/utils/schema-validator.js
@@ -0,0 +1,23 @@
+const validateRequest = (contextPart, label, schema, options) => {
+  if (!schema) return
+  const { error } = schema.validate(contextPart, options)
+  if (error) {
+    throw new Error(`Invalid ${label} - ${error.message}`)
+  }
+}
+// middleware de validacion que usaremos para validar los request pasandole un determinado esquema
+const validate = (schema) => (ctx, next) => {
+  try {
+    validateRequest(ctx.headers, 'Headers', schema.headers, { allowUnknown: true })
+    validateRequest(ctx.params, 'URL Parameters', schema.params)
+    validateRequest(ctx.query, 'URL Query', schema.query)
+    if (ctx.request.body) {
+      validateRequest(ctx.request.body, 'Request Body', schema.body)
+    }
+    return next()
+  } catch (error) {
+    ctx.throw(422, error.message)
+  }
+}
+
+module.exports = validate
